d4abdab543fb959201dfb0dc665f6a3369494e0e
Ubantu 20.4 <1174639913@qq.com>
2020-11-26 10:25:01 +0800

    got pages added

---
+++ src/routes/carUsage.svelte
@@ -0,0 +1,44 @@
+<script context="module">
+	import requests from '../data/requests.js';
+
+	export async function preload(){
+		try {
+			const carbookStats = await requests.carbookStats();
+			const historicUS = await requests.historicUS();
+			const statesData = await requests.statesData();
+			return {carbookStats,historicUS,statesData};
+		} catch (error) {
+			this.error(500,"There is an error in calling the api,please try again in 5 minutes.");
+			return;
+		}
+	}
+</script>
+
+<script>
+	import Carplate from '../components/Carplate.svelte';
+	import Carusage from '../components/Carusage.svelte';
+	import Carbookuser from '../components/Carbookuser.svelte';
+	import TableContainer from '../components/TableContainer.svelte';
+import About from './about.svelte';
+import Error from './_error.svelte';
+import Layout from './_layout.svelte';
+
+	export let carbookStats;
+	export let historicUS;
+	export let statesData;
+</script>
+
+<svelte:head>
+    <title>Car book system</title>
+</svelte:head>
+
+<div class="section header">
+	<div class="container">
+		<h1>Car book system</h1>
+	</div>
+</div>
+
+<Carplate />
+<Carusage {...carbookStats}/>
+<Carbookuser historicData={historicUS} title="Covid-19" />
+<TableContainer data={statesData}/>
